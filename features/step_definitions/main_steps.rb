002cca016e8ef638d4c6687076a87661f08c708a
Nzakou Cédric <nzakou.cedric@protonmail.com>
2018-06-24 17:58:03 +0200

    add pending steps and move log to fixtures

---
+++ features/step_definitions/main_steps.rb
@@ -0,0 +1,23 @@
+Given("a file log files") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("I parse the file") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("I sort the file by most views") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I output the result by most views") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("I sort the file by most unique page views") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I output the result by most unique page views") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
