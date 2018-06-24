e5b50af4344f4cf6848483c0ac857bbbc9b4601e
Nzakou Cédric <nzakou.cedric@protonmail.com>
2018-06-25 00:53:01 +0200

    add input out put

--- lib/smart_test.rb
+++ lib/smart_test.rb
@@ -1,8 +1,17 @@
 #!/usr/bin/env ruby
-require "smart_test/version"
-require "smart_test/mapper"
-require "smart_test/sort"
+require_relative "smart_test/version"
+require_relative "smart_test/mapper"
+require_relative "smart_test/sort"
 
 module SmartTest
-  # Your code goes here...
+  def self.run(args = nil)
+    path       =  args[0]
+    parsed_log = Mapper.new(path: path).parse
+    if args[1] == "uniq"
+      sorted_log = Sort.new(log: parsed_log).sort_by_most_unique_page_views
+    else
+      sorted_log = Sort.new(log: parsed_log).sort_by_most_views
+    end
+    puts sorted_log
+  end
 end
